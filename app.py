1079a5351c73fb196bed5b8bc16c451cfaba2d5f
Nicholas Gonzalez <41881164+nickgonzalez42@users.noreply.github.com>
2023-02-25 18:41:50 +0000

    created jobitem template

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Chicago',
+    'salary': 100000
+  },
+  {
+    'id': 2,
+    'title': 'Clown',
+    'location': 'Austin',
+    'salary': 1000000
+  },
+  {
+    'id': 3,
+    'title': 'Professor',
+    'location': 'Atlanta',
+    'salary': 150000
+  },
+  {
+    'id': 4,
+    'title': 'Influencer',
+    'location': 'Washington D.C.',
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                        jobs=JOBS)
+
+# returns JSON
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
