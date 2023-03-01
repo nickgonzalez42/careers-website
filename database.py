9ffea4d220d41058c358a1c7ef5bf2f3c5085435
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-03-01 10:56:27 -0600

    ANOTHER minor fix

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 
+# need to hide this in the future
 db_connection_string = "mysql+pymysql://nsxwgri0aylim8kwcvjf:pscale_pw_XNoiyel7ZGcRlv9oqBOEUrpdtkxMaUmJOFCo6c1yvv4@us-east.connect.psdb.cloud/careers?charset=utf8mb4"
 
 engine = create_engine(
