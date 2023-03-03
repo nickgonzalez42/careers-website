d4898374ffe1854f4a8619448c3ba71e4836f9ee
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-03-03 08:45:26 -0600

    dynamic show pages

--- database.py
+++ database.py
@@ -21,3 +21,15 @@ def load_jobs():
 		for row in result_all:
 			jobs.append(dict(zip(column_names, row)))
 		return jobs
+		
+def load_job_from_db(id):
+	with engine.connect() as conn:
+		result = conn.execute(
+			text(f"SELECT * FROM jobs WHERE id = {id}")
+		)
+		rows = result.all()
+		if len(rows) == 0:
+			return None;
+		else: 
+			column_names = result.keys()
+			return dict(zip(column_names, rows[0]))
